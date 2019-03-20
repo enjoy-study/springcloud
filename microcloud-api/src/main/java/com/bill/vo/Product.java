dfb51e3233660cd0ce08cd520ec4d5f890206402
developerli <developerli@126.com>
2019-03-20 16:55:23 +0800

    20190320 initial commit

---
+++ microcloud-api/src/main/java/com/bill/vo/Product.java
@@ -0,0 +1,9 @@
+package com.bill.vo;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private Long productId;
+    private String productName;
+    private String productDesc;
+}
