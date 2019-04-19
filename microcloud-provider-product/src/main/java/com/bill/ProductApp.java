17e9ee0a67b58b734231a2919409d444643a955b
developerli <developerli@126.com>
2019-04-19 13:26:56 +0800

    20190419 spring cloud

--- microcloud-provider-product/src/main/java/com/bill/ProductApp.java
+++ microcloud-provider-product/src/main/java/com/bill/ProductApp.java
@@ -3,9 +3,11 @@ package com.bill;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.bill.mapper")
+@EnableEurekaClient
 public class ProductApp {
 
     public static void main(String[] args) {
